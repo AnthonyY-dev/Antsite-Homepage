9d9546cb273cf2d2063fc9d21da35c8e2837d1d5
Anthony Y <anthony@antsite.xyz>
2024-03-07 12:21:33 -0500

    made navbar sticky and transparent, added logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import Logo from "../assets/Logo.png";
 
 const Navbar = () => {
   return (
-    <header className="tw-sticky tw-top-0 tw-bg-red-600 tw-p-4 tw-flex tw-flex-row">
+    <header className="tw-sticky tw-top-0 tw-p-4 tw-flex tw-flex-row navbar">
       <img src={Logo} alt="My Logo" className="tw-size-8 tw-p-0" />
       Test
     </header>
