0a3974cca65069f8ee30a670b5a55db33bdd5adb
Anthony Y <=anthony@antsite.xyz>
2024-03-07 20:03:31 -0500

    Everything else was crap, lets just count this as the initial commit.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { BackgroundBeams } from "./components/Beams";
 import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <>
       <Navbar></Navbar>
+      <BackgroundBeams></BackgroundBeams>
     </>
   );
 };
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,9 +2,11 @@ import Logo from "../assets/Logo.png";
 
 const Navbar = () => {
   return (
-    <header className="tw-sticky tw-top-0 tw-p-4 tw-flex tw-flex-row navbar">
-      <img src={Logo} alt="My Logo" className="tw-size-8 tw-p-0" />
-      Test
+    <header className="tw-sticky tw-top-0 navbar">
+      <div className="tw-flex tw-flex-row tw-gap-2 tw-ml-2">
+        <img src={Logo} alt="My Logo" className="tw-size-8 tw-p-0" />
+        <p className="tw-font-semibold tw-mt-0.5">antsite.xyz</p>
+      </div>
     </header>
   );
 };
